781bd4c47e45c648ce35c054a62d4f7c01b0aebb
Mynor Reyes <mynorrb@gmail.com>
2020-06-15 15:53:03 -0600

    actualización llave

--- server/config/config.js
+++ server/config/config.js
@@ -42,4 +42,4 @@ process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
 // Google client id
 //==============
 
-process.env.CLIENT_ID = process.env.CLIENT_ID || '72919168886-k3lqcporcsn3lks0emlj5lfjg19p2dtt.apps.googleusercontent.com';
+process.env.CLIENT_ID = process.env.CLIENT_ID || '72919168886-7qj95jli4qcfaarv948oi4u26jooml47.apps.googleusercontent.com';
